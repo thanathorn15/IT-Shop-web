1bcc989acd2a91f5adfaba29c18466097fed7db7
Thanathorn15 <thanathorn_ko@kkumail.com>
2023-06-04 21:33:52 +0700

    Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Link,NavLink,BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import MyOrder from './pages/MyOrder'
+import Login from './pages/Login'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
+    <BrowserRouter>
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <div>
+         <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">IT Shop</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1 font-semibold">
+      <li><Link to= '/'>Home</Link></li>
+      <li><Link to = '/product'>Products</Link></li>
+      <li><Link to = '/myorder'>MyOrder</Link></li>
+      <li><Link to = '/login'>Login</Link></li>
+      
+    </ul>
+  </div>
+</div>
+    </div>
+
+<Routes>
+  <Route path='/' element={<Home/>} />
+  <Route path='/product' element={<Products/>} />
+  <Route path='/myorder' element={<MyOrder/>} />
+  <Route path='/login' element={<Login/>} />
+ 
+ 
+</Routes>
     </>
+</BrowserRouter>
   )
 }
 
---
+++ src/components/Banner.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div>
+         <div className='flex justify-center'>
+    <div className="carousel w-[60rem] ">
+  <div id="item1" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1544731612-de7f96afe55f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" className="w-full" />
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" className="w-full" />
+  </div> 
+  <div id="item3" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" className="w-full" />
+  </div> 
+  <div id="item4" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80" className="w-full" />
+  </div>
+  </div>
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>
+    </div>
+  )
+}
